ccc2611f84bc34e3861acc5075a1020a52974eb4
lailahamdan1116 <94996494+lailahamdan1116@users.noreply.github.com>
2022-04-19 20:44:08 -0500

    commit

---
+++ Unit Testing Activity/unit testing.js
@@ -0,0 +1,17 @@
+
+function Greet(name){
+   if(!name){
+      console.log("Hello there!");
+   }else{
+   console.log("Hello, " + name + ".");
+   }
+  if(name.toUpperCase() === name && name === String){
+    console.log("HELLO," + name + ".");
+  }
+   
+
+}
+console.log(Greet);
+Greet("LAILA");
+
+
